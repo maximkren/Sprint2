b752783eb9c3292a46177f67ba2984483b0af5f9
Maxim Kren <krenmaxim@gmail.com>
2018-08-07 11:23:33 +0300

    Initial commit

---
+++ src/main/info/sjd/runners/RunnerSumOfDigits.java
@@ -0,0 +1,13 @@
+package main.info.sjd.runners;
+
+import java.util.logging.Logger;
+
+public class RunnerSumOfDigits {
+
+	private static Logger logger = Logger.getLogger(RunnerSumOfDigits.class.getName());
+
+	public static void main(String[] args) {
+
+	}
+
+}
